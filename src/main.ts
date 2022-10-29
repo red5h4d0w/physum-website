191054dbca918ac5854b099825b64eaa67207b28
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-10-29 11:32:24 -0400

    fix some collapsing stuff

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import './app.css'
 import App from './App.svelte'
 import './scss/styles.scss'
+import  $ from 'jquery'
+import * as bootstrap from 'bootstrap'
 import './assets/icomoon/style.css'
+
 
 
 const app = new App({
